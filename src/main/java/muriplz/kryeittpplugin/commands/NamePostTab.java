a6f636080d4ce12ab89b570be69c08270df5f81f
muriplz <46825658+muriplz@users.noreply.github.com>
2021-05-05 21:06:15 +0200

    return an empty list rather than null on NamePostTab

--- src/main/java/muriplz/kryeittpplugin/commands/NamePostTab.java
+++ src/main/java/muriplz/kryeittpplugin/commands/NamePostTab.java
@@ -6,11 +6,13 @@ import org.bukkit.command.TabCompleter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NamePostTab implements TabCompleter {
     @Nullable
     @Override
     public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
-        return null;
+        List myList = new ArrayList();
+        return myList;
     }}
