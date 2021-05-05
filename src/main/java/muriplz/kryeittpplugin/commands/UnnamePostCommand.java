6abe6dee74431a7c0f069466fc4385c81f3f30cc
muriplz <46825658+muriplz@users.noreply.github.com>
2021-05-05 17:27:52 +0200

    Named Post System (75%)

--- src/main/java/muriplz/kryeittpplugin/commands/UnnamePostCommand.java
+++ src/main/java/muriplz/kryeittpplugin/commands/UnnamePostCommand.java
@@ -1,19 +1,14 @@
 package muriplz.kryeittpplugin.commands;
 
 import io.github.niestrat99.advancedteleport.api.Warp;
-import io.github.niestrat99.advancedteleport.sql.WarpSQLManager;
 import muriplz.kryeittpplugin.KryeitTPPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.HashMap;
-import java.util.Set;
 
 public class UnnamePostCommand implements CommandExecutor {
 
@@ -34,76 +29,18 @@ public class UnnamePostCommand implements CommandExecutor {
                 player.sendMessage("Use /UnnamePost <PostName> to unname the nearest post.");
                 return false;
             }
-            if(!player.hasPermission("telepost.namepost")){
+            if(!player.hasPermission("telepost.unnamepost")){
                 player.sendMessage(ChatColor.RED+"You don't have permission to use this command.");
-                return false;
-            }
-            if(!player.getWorld().getName().equals("world")){
-                player.sendMessage(ChatColor.RED+"You have to be in the Overworld to use this command.");
                 return false;
             }
             if(args.length==1){
                 if(Warp.getWarps().containsKey(args[0])){
-                    player.sendMessage(ChatColor.RED+"The post "+args[0]+" already exists, try a different name or un-name the post using /unnamePost Agua.");
+                    
                     return false;
-                }
-
-                int gap = plugin.getConfig().getInt("distance-between-posts");
-                int originX = plugin.getConfig().getInt("post-x-location");
-                int originZ = plugin.getConfig().getInt("post-z-location");
-                int playerX = player.getLocation().getBlockX()-originX;
-                int playerZ = player.getLocation().getBlockZ()-originZ;
-                //para el eje X
-                int postX=0;
-                while(true){
-                    if(playerX>=gap && playerX>0){
-                        playerX=playerX-gap;
-                        postX+=gap;
-                    }
-                    else if(playerX<=-gap && playerX<0){
-                        playerX=playerX+gap;
-                        postX-=gap;
-                    }
-                    else{break;}
-                }
-                if(playerX>gap/2&&playerX>0){
-                    postX+=gap;
-                }
-                if(playerX<-gap/2&&playerX<0){
-                    postX-=gap;
-                }
-                //para el eje Z
-                int postZ=0;
-                while(true){
-                    if(playerZ>=gap && playerZ>0){
-                        playerZ=playerZ-gap;
-                        postZ+=gap;
-                    }
-                    else if(playerZ<=-gap && playerZ<0){
-                        playerZ=playerZ+gap;
-                        postZ-=gap;
-                    }
-                    else{break;}
-                }
-                if(playerZ>gap/2&&playerZ>0){
-                    postZ+=gap;
-                }
-                if(playerZ<-gap/2&&playerZ<0){
-                    postZ-=gap;
-                }
-                postX+=originX;
-                postZ+=originZ;
-                Location nearestpostLocation = new Location(player.getWorld(), postX + 0.5, 260, postZ + 0.5);
-                HashMap<String, Warp> warps = Warp.getWarps();
-                Set<String> warpNames = warps.keySet();
-                for(String warpName: warpNames){
-                    if(Warp.getWarps().get(warpName).getLocation()==nearestpostLocation){
-                        player.sendMessage(ChatColor.RED+"The nearest post is already named, it's "+warpName+".");
-                        return false;
-                    }
+                }else{
+                    player.sendMessage(ChatColor.RED+"No posts by that name");
                 }
             }
-
             return false;
         }
     }}
