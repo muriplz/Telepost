711a20b8e2384dbd76d04eb1e35e4dc0ce864572
muriplz <46825658+muriplz@users.noreply.github.com>
2021-06-24 23:07:07 +0200

    fixed /posthelp tab

--- src/main/java/muriplz/kryeittpplugin/tabCompletion/HelpTab.java
+++ src/main/java/muriplz/kryeittpplugin/tabCompletion/HelpTab.java
@@ -15,38 +15,41 @@ public class HelpTab implements TabCompleter {
     @Override
     public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
 
-        // Getting the Player
-        Player player = (Player) sender;
-
-        // Creating the lists with the autocomplete text
-        List<String> commands = new ArrayList<>();
         List<String> completions = new ArrayList<>();
+        if(args.length==1){
+            // Getting the Player
+            Player player = (Player) sender;
 
-        // Adding the autocomplete text
-        if(player.hasPermission("telepost.buildpost")){
-            commands.add("buildpost");
-        }
-        if(player.hasPermission("telepost.namepost")){
-            commands.add("namepost");
-        }
-        if(player.hasPermission("telepost.unnamepost")){
-            commands.add("unnamepost");
-        }
-        commands.add("aliases");
-        commands.add("nearestpost");
-        commands.add("setpost");
-        commands.add("homepost");
-        commands.add("visit");
-        commands.add("postlist");
-        commands.add("invite");
+            // Creating the lists with the autocomplete text
+            List<String> commands = new ArrayList<>();
 
-        // Add to "completions" all words that have letters that are contained on "commands" list
-        int i=0;
-        while(i < commands.size()){
-            if(commands.get(i).toLowerCase().contains(args[0]) || commands.get(i).contains(args[0])) {
-                completions.add(commands.get(i));
+
+            // Adding the autocomplete text
+            if(player.hasPermission("telepost.buildpost")){
+                commands.add("buildpost");
             }
-            i++;
+            if(player.hasPermission("telepost.namepost")){
+                commands.add("namepost");
+            }
+            if(player.hasPermission("telepost.unnamepost")){
+                commands.add("unnamepost");
+            }
+            commands.add("aliases");
+            commands.add("nearestpost");
+            commands.add("setpost");
+            commands.add("homepost");
+            commands.add("visit");
+            commands.add("postlist");
+            commands.add("invite");
+
+            // Add to "completions" all words that have letters that are contained on "commands" list
+            int i=0;
+            while(i < commands.size()){
+                if(commands.get(i).toLowerCase().contains(args[0]) || commands.get(i).contains(args[0])) {
+                    completions.add(commands.get(i));
+                }
+                i++;
+            }
         }
         return completions;
     }
