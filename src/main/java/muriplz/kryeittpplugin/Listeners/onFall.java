38844ad17c15c38a4304f6691e4f772ed133233c
muriplz <46825658+muriplz@users.noreply.github.com>
2021-08-17 04:39:52 +0200

    now the player does not take fall damage

--- src/main/java/muriplz/kryeittpplugin/Listeners/onFall.java
+++ src/main/java/muriplz/kryeittpplugin/Listeners/onFall.java
@@ -1,9 +1,11 @@
 package muriplz.kryeittpplugin.Listeners;
 
+import javafx.scene.layout.Priority;
 import muriplz.kryeittpplugin.KryeitTPPlugin;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 
@@ -11,7 +13,7 @@ public class onFall implements Listener {
 
     public KryeitTPPlugin plugin = KryeitTPPlugin.getInstance();
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void preventFirstFall(EntityDamageEvent event) {
         if (event.getEntityType() == EntityType.PLAYER) {
             Player player = (Player) event.getEntity();
